61b9f00c4b55c8abd530c82824adb7b26f1f9fbd
Juho G <jgavert@users.noreply.github.com>
2016-02-04 02:23:43 +0200

    proper fixes graph working

--- appspace/utils/graph_compute.hlsl
+++ appspace/utils/graph_compute.hlsl
@@ -16,9 +16,13 @@ void CSMain(uint3 DTid : SV_DispatchThreadID)
 	float4 finalColor = float4(0.8f, 0.1f, 0.1f, 0.6f);
 	float pixelHeight = (consta.val - consta.valueMin) / (consta.valueMax - consta.valueMin);
 	float drawedPixelHeight = (int)((float)consta.height) * pixelHeight;
-	if (drawedPixelHeight - uv.y < 0.1)
+	if (abs(drawedPixelHeight - uv.y) < 0.8)
 	{
 		finalColor = float4(1.f, 1.f, 1.f, 1.f);
+	}
+	else if (abs(drawedPixelHeight - uv.y) < 1.5)
+	{
+		finalColor = float4(0.9f, 0.5f, 0.5f, 0.6f);
 	}
 	// need to get texture pos, look at min/max if the val is in the same y coordinate, and put it white.
 	// otherwise put alpha 0.0 to make it transparent. (I guess)
