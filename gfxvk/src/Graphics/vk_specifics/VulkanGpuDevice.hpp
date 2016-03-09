6daabcd83f4afdfd2789de271891e907546342b8
Juho G <nomail>
2016-03-09 02:26:31 +0200

    Added bunch of queue logic.
    Should handle giving right kind of queues for every device.
    Abort when things are strange.

--- gfxvk/src/Graphics/vk_specifics/VulkanGpuDevice.hpp
+++ gfxvk/src/Graphics/vk_specifics/VulkanGpuDevice.hpp
@@ -8,13 +8,28 @@ class VulkanGpuDevice
 {
 private:
 
+
   vk::AllocationCallbacks m_alloc_info;
   FazPtrVk<vk::Device>    m_device;
   bool                    m_debugLayer;
   std::vector<vk::QueueFamilyProperties> m_queues;
+  bool                    m_singleQueue;
+  bool                    m_onlySeparateQueues;
+  FazPtrVk<vk::Queue>     m_internalUniversalQueue;
+  struct FreeQueues
+  {
+    int universalIndex;
+    int graphicsIndex;
+    int computeIndex;
+    int dmaIndex;
+    std::vector<uint32_t> universal;
+    std::vector<uint32_t> graphics;
+    std::vector<uint32_t> compute;
+    std::vector<uint32_t> dma;
+  } m_freeQueueIndexes;
+
 public:
   VulkanGpuDevice(FazPtrVk<vk::Device> device, vk::AllocationCallbacks alloc_info, std::vector<vk::QueueFamilyProperties> queues, bool debugLayer);
-  ~VulkanGpuDevice();
   VulkanQueue createDMAQueue();
   VulkanQueue createComputeQueue();
   VulkanQueue createGraphicsQueue();
