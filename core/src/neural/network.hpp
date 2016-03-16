47b7d3415513746e11fae1d4d6d1b23bb3d86e37
Juho G <jgavert@users.noreply.github.com>
2016-03-16 15:24:26 +0200

    Started on my own basic neural network

---
+++ core/src/neural/network.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace faze
+{
+  void testNetwork();
+  class NeuralNetwork 
+  {
+	  size_t m_inputLayerSize;
+	  size_t m_outputLayerSize;
+	  size_t m_hiddenLayerSize;
+
+  };
+}
