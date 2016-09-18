01f69da7efb77167a270862b419f5ff5460770fd
Juho G <jgavert@users.noreply.github.com>
2016-09-19 02:36:47 +0300

    Finetuned MemView and made CopyBufferPacket All packets get allocator as first parameter if they need more memory for their info. Lots of tuning of MemView, but I fear it's not enough yet. TODO: Add MemView to be used almost everywhere as it's lightweight view into some memory. Like filesystem.

--- gfxvk/src/Graphics/vk_specifics/VulkanCmdBuffer.cpp
+++ gfxvk/src/Graphics/vk_specifics/VulkanCmdBuffer.cpp
@@ -1,6 +1,36 @@
 #include "VulkanCmdBuffer.hpp"
 
+#include "core/src/global_debug.hpp"
 #include "VulkanBuffer.hpp"
+
+using namespace faze;
+
+class BufferCopyPacket : public VulkanCommandPacket
+{
+private:
+  std::shared_ptr<vk::Buffer> from;
+  std::shared_ptr<vk::Buffer> to;
+  CommandListVector<vk::BufferCopy> m_copyList;
+public:
+
+  BufferCopyPacket(LinearAllocator& allocator, std::shared_ptr<vk::Buffer> from, std::shared_ptr<vk::Buffer> to, MemView<vk::BufferCopy> copyList)
+    : from(from)
+    , to(to)
+    , m_copyList(std::forward<decltype(copyList)>(MemView<vk::BufferCopy>(allocator.allocList<vk::BufferCopy>(copyList.size()), copyList.size())))
+  {
+    F_ASSERT(copyList, "Invalid copy commands");
+    for (size_t i = 0; i < m_copyList.size(); i++)
+    {
+      m_copyList[i] = copyList[i];
+    }
+  }
+  void execute(vk::CommandBuffer& buffer) override
+  {
+    vk::ArrayProxy<const vk::BufferCopy> proxy(static_cast<uint32_t>(m_copyList.size()), m_copyList.data());
+    buffer.copyBuffer(*from, *to, proxy);
+  }
+};
+
 
 VulkanCmdBuffer::VulkanCmdBuffer(std::shared_ptr<vk::CommandBuffer> buffer, std::shared_ptr<vk::CommandPool> pool)
   : m_cmdBuffer(std::forward<decltype(buffer)>(buffer)), m_pool(std::forward<decltype(pool)>(pool)), m_closed(false)
@@ -26,27 +56,8 @@ bool VulkanCmdBuffer::isClosed()
 {
   return m_closed;
 }
-
-class CopyBufferToBuffer : public CommandPacket
-{
-private:
-  std::shared_ptr<vk::Buffer> from;
-  std::shared_ptr<vk::Buffer> to;
-public:
-
-  CopyBufferToBuffer(std::shared_ptr<vk::Buffer> from, std::shared_ptr<vk::Buffer> to)
-    : from(from)
-    , to(to)
-  {
-    
-  }
-  void execute() override
-  {
-
-  }
-};
-
 void VulkanCmdBuffer::copy(VulkanBuffer from, VulkanBuffer to)
 {
-  m_commandList.insert<CopyBufferToBuffer>(from.m_resource, to.m_resource);
+  vk::BufferCopy copy;
+  m_commandList.insert<BufferCopyPacket>(from.m_resource, to.m_resource, copy);
 }
