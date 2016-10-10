661310d7aa20196785f5f4b2194fc0f42f59340e
Juho G <nomail>
2016-10-10 20:06:01 +0300

    started better format conversion

--- gfxvk/src/Graphics/vk_specifics/VulkanFormat.hpp
+++ gfxvk/src/Graphics/vk_specifics/VulkanFormat.hpp
@@ -5,20 +5,27 @@
 #include <vulkan/vulkan.hpp>
 
 
-static vk::Format formatToVkFormat[] =
+struct FormatVulkanConversion 
 {
-	vk::Format::eUndefined,
-	vk::Format::eR32G32B32A32Uint,
-	vk::Format::eR32G32B32A32Sfloat,
-	vk::Format::eR32G32B32Uint,
-	vk::Format::eR32G32B32Sfloat,
-	vk::Format::eR8G8B8A8Uint,
-	vk::Format::eR8G8B8A8Unorm,
-	vk::Format::eR8G8B8A8Srgb,
-	vk::Format::eR32Sfloat,
-	vk::Format::eD32Sfloat,
-	vk::Format::eR8Uint,
-	vk::Format::eR8Unorm,
+	vk::Format storage;
+	vk::Format view;
+	FormatType enm;
+};
+
+static FormatVulkanConversion formatToVkFormat[] =
+{
+	{, vk::Format::eUndefined, },
+	{, vk::Format::eR32G32B32A32Uint, },
+	{, vk::Format::eR32G32B32A32Sfloat, },
+	{, vk::Format::eR32G32B32Uint, },
+	{, vk::Format::eR32G32B32Sfloat, },
+	{, vk::Format::eR8G8B8A8Uint, },
+	{, vk::Format::eR8G8B8A8Unorm, },
+	{, vk::Format::eR8G8B8A8Srgb, },
+	{, vk::Format::eR32Sfloat, },
+	{, vk::Format::eD32Sfloat, },
+	{, vk::Format::eR8Uint, },
+	{, vk::Format::eR8Unorm, },
 };
 
 namespace faze
