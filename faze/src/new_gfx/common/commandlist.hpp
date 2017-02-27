b7444fd6d5302d9c6781e81a3b3c3c66943cd669
Juho G <jgavert@users.noreply.github.com>
2017-02-28 00:56:06 +0200

    Started on commandlists compiles Need to expose all kinds of things now. Going towards buffer copy, from there, swapchain. Implementing fast upload things later.

---
+++ faze/src/new_gfx/common/commandlist.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "intermediatelist.hpp"
+#include "core/src/global_debug.hpp"
+
+namespace faze
+{
+  static constexpr const int CommandlistSize = 1024 * 1024;
+
+  class DmaList
+  {
+    friend class ComputeList;
+    friend class GraphicsList;
+    IntermediateList list;
+  public:
+    DmaList()
+      : list(LinearAllocator(CommandlistSize))
+    {
+
+    }
+  };
+
+  class ComputeList : public DmaList
+  {
+    friend class GraphicsList;
+  public:
+    ComputeList()
+      : DmaList()
+    {}
+
+  };
+
+  class GraphicsList : public ComputeList
+  {
+  public:
+    GraphicsList()
+      : ComputeList()
+    {}
+  };
+}
