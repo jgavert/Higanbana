42066ca02551d077efd17626577a403e672f8dd0
Juho G <jgavert@users.noreply.github.com>
2017-03-03 15:30:43 +0200

    re-enabled alt+enter, changed my binding to other. alt+enter is different from dxgi alt+enter behavior.

--- faze/src/new_gfx/dx12/dx12gpudevice.cpp
+++ faze/src/new_gfx/dx12/dx12gpudevice.cpp
@@ -214,7 +214,7 @@ namespace faze
       FAZE_CHECK_HR(dxgiFactory->CreateSwapChainForHwnd(m_graphicsQueue.Get(), natSurface->native(), &swapChainDesc, &fullDesc, nullptr, (IDXGISwapChain1**)&swapChain));
       auto sc = std::make_shared<DX12Swapchain>(swapChain, *natSurface);
       sc->setBufferMetadata(width, height, bufferCount, format, mode);
-      dxgiFactory->MakeWindowAssociation(natSurface->native(), DXGI_MWA_NO_WINDOW_CHANGES);
+      //dxgiFactory->MakeWindowAssociation(natSurface->native(), DXGI_MWA_NO_WINDOW_CHANGES);
       return sc;
     }
 
