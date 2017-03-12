fcdcf0a348e56100dca80c885ea75c5f46a08605
Juho G <jgavert@users.noreply.github.com>
2017-03-12 19:45:20 +0200

    Vs2017 was quite painless to get working. Seems like future updates might be to only change one number... possibly! and not all folders and locations :)

--- generateConfig.py
+++ generateConfig.py
@@ -2,17 +2,17 @@
 import os
 
 config = """
-.VSBasePath     = 'C:/Program Files (x86)/Microsoft Visual Studio 14.0'
+.VSBasePath     = 'C:/Program Files (x86)/Microsoft Visual Studio/2017/Community'
 .WindowsSDKBasePath10 = 'C:/Program Files (x86)/Windows Kits/10'
 .WindowsSDKSubVersion = '10.0.14393.0'
 #if __WINDOWS__
 .FazEPath = 'CURRENT_DIRECTORY'
-.FBuildCache = 'C:/temp/cache'
+.FBuildCache = 'C:/temp/fazecache'
 .VulkanSDKBasePath = 'C:/VulkanSDK/1.0.42.0'
 #endif
 #if __LINUX__
 .FazEPath = 'CURRENT_DIRECTORY'
-.FBuildCache = '/tmp/.fbuild.cache'
+.FBuildCache = '/tmp/.fbuild.fazecache'
 .VulkanSDKBasePath = '/usr/lib'
 #endif"""
 
