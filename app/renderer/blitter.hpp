da65680a561d48fafb205d5201582350838434db
Juho G <jgavert@users.noreply.github.com>
2017-08-17 03:52:52 +0300

    New modes for blitter

--- app/renderer/blitter.hpp
+++ app/renderer/blitter.hpp
@@ -10,9 +10,18 @@ namespace faze
       GraphicsPipeline pipeline;
       Renderpass renderpass;
     public:
+      enum class FitMode
+      {
+        Fill,
+        Fit,
+        Stretch
+      };
+
       Blitter(GpuDevice& device);
 
       void blit(GpuDevice& device, CommandGraph& graph, TextureRTV& target, TextureSRV& source, float2 topleft, float2 size);
+      void blit(GpuDevice& device, CommandGraph& graph, TextureRTV& target, TextureSRV& source, int2 topleft, int2 size);
+      void blitImage(GpuDevice& device, CommandGraph& graph, TextureRTV& target, TextureSRV& source, FitMode mode);
     };
   }
 }
