ae0b4239b88cd383a46c6812d107cb7ffb1f519c
Juho G <jgavert@users.noreply.github.com>
2017-12-23 03:24:56 +0200

    missing dll copy

--- compileFinal.bat
+++ compileFinal.bat
@@ -2,4 +2,5 @@ CALL FBuild.exe -dist App-x64-Release
 rmdir /s /q package
 mkdir package
 ROBOCOPY bin/x64-Release/App/ package/ App.exe
+ROBOCOPY bin/x64-Release/App/ package/ *.dll
 ROBOCOPY appspace/ package/ /E
