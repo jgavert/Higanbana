a7577308e8b6485b0999fba3e5c87183c3f3bd1c
Juho G <jgavert@users.noreply.github.com>
2018-01-14 05:00:36 +0200

    Added some log to new main

--- app/EntryPoint.cpp
+++ app/EntryPoint.cpp
@@ -4,6 +4,7 @@
 
 #include "core/src/system/LBS.hpp"
 #include "core/src/system/time.hpp"
+#include "core/src/system/logger.hpp"
 #include "faze/src/new_gfx/GraphicsCore.hpp"
 
 using namespace faze;
@@ -12,9 +13,12 @@ using namespace faze::math;
 int EntryPoint::main()
 {
   //mainWindow(m_params);
+  Logger log;
   FileSystem fs;
   GraphicsSubsystem graphics(GraphicsApi::Vulkan, "faze");
   auto gpuInfo = graphics.getVendorDevice(VendorID::Amd);
   auto gpu = graphics.createDevice(fs, gpuInfo);
+  F_LOG("gpu \"%s\" %s apiVersion: %s %zdMB\n", gpuInfo.name.c_str(), gpuInfo.canPresent ? "can present" : "cannot present", gpuInfo.apiVersionStr.c_str(), gpuInfo.memory / 1024 / 1024);
+  log.update();
   return 0;
 }
