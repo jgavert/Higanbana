d6c16ee01115e1610c1cf1cf14004f31dda503ae
Juho G <jgavert@users.noreply.github.com>
2018-01-14 02:23:11 +0200

    Some more fixes on linux

---
+++ compileDebug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./fbuild app-x64linux-debug
+#mkdir -p workspace
+#rm -rf workspace/*
+#cp bin/x64Linux-Vulkan-Debug/App/App workspace/.
+#cd workspace
+#cp -r ../appspace/* .
+#ln -s ../appspace/* .
+
+
+
