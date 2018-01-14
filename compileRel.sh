79b72ad348bcda3b9e8c282be262f7fe0f79b5eb
Juho Gävert <jgavert@nomail>
2018-01-14 02:23:11 +0200

    Some more fixes on linux

---
+++ compileRel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./fbuild  app-x64linux-release
+#mkdir -p workspace
+#rm -rf workspace/*
+#cp bin/x64Linux-Release/App/App workspace/.
+#cd workspace
+#cp -r ../appspace/* .
+#ln -s ../appspace/* .
+
+
+
