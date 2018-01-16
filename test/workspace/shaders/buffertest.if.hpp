c0da4d3830ba7d10aefc37140a79c1186d62e6d3
Juho G <nomail>
2018-01-16 03:09:17 +0200

    missing shaders

---
+++ test/workspace/shaders/buffertest.if.hpp
@@ -0,0 +1,21 @@
+#ifndef BUFFERTEST_HLSL
+#define BUFFERTEST_HLSL
+
+#include "definitions.hpp"
+
+FAZE_BEGIN_LAYOUT(BufferTest)
+
+FAZE_CBUFFER
+{
+  float _unused;
+};
+
+FAZE_SRV(Buffer<uint>, input, 0)
+FAZE_UAV(RWBuffer<uint>, output, 0)
+
+FAZE_SRV_TABLE(1)
+FAZE_UAV_TABLE(1)
+
+FAZE_END_LAYOUT
+
+#endif /*FAZE_SHADER_DEFINITIONS*/
