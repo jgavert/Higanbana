7b7eaed508746e028ff3da647da3e248862838b4
Juho G <jgavert@users.noreply.github.com>
2018-01-16 03:09:17 +0200

    missing shaders

---
+++ test/workspace/shaders/triangle.ps.hlsl
@@ -0,0 +1,17 @@
+#include "triangle.if.hpp"
+
+struct PSInput
+{
+  float2 uv : TEXCOORD0;
+  float4 pos : SV_Position;
+};
+
+GX_SIGNATURE
+float4 main(PSInput input) : SV_Target
+{
+  float4 final = color;
+  final += yellow.Sample(staSam, input.uv);
+  //final.xy = input.uv;
+  //final.zw = float2(0.f, 0.f);
+  return final;
+}
