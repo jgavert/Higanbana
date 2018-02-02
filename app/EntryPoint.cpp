81319d003501ecef3b5c18104d598d6d6685d87f
Juho G <nomail>
2018-02-02 13:01:51 +0200

    Small gamejam challenge Basically Ill use this to make a simple 2d fighter Time for test if it's possible with my libraries. Already got mainloop rolling with a total of 4 threads !

--- app/EntryPoint.cpp
+++ app/EntryPoint.cpp
@@ -1,6 +1,7 @@
 #include "core/src/Platform/EntryPoint.hpp"
 
-#include "windowMain.hpp"
+//#include "windowMain.hpp"
+#include "fighterMain.hpp"
 
 #include "core/src/system/LBS.hpp"
 #include "core/src/system/time.hpp"
@@ -12,17 +13,7 @@ using namespace faze::math;
 
 int EntryPoint::main()
 {
-  mainWindow(m_params);
-  
-  Logger log;
-  FileSystem fs;
-  GraphicsSubsystem graphics(GraphicsApi::DX12, "faze");
-  auto gpuInfo = graphics.getVendorDevice(VendorID::Amd);
-  {
-      auto gpu = graphics.createDevice(fs, gpuInfo);
-      F_LOG("gpu \"%s\" %s apiVersion: %s %zdMB\n", gpuInfo.name.c_str(), gpuInfo.canPresent ? "can present" : "cannot present", gpuInfo.apiVersionStr.c_str(), gpuInfo.memory / 1024 / 1024);
-  }
-  log.update();
-  
+  fighterWindow(m_params);
+
   return 0;
 }
--- app/testRenderer.hpp
+++ app/testRenderer.hpp
@@ -95,8 +95,94 @@ namespace faze
       postEffectLDSUav = gpu.createTextureUAV(postEffectLDSTex);
     }
 
-    void render(TextureRTV target)
+    void render()
     {
+      /*
+      CommandGraph tasks = gpu.createGraph();
+
+      raymarchTexture.next();
+      uint2 iRes = raymarchTexture.desc().size().xy();
+      {
+        auto type = CommandGraphNode::NodeType::Graphics;
+
+        auto node = tasks.createPass("compute!", type);
+
+        auto binding = node.bind<::shader::TextureTest>(raymarcher);
+        binding.constants = {};
+        binding.constants.iResolution = iRes;
+        binding.constants.iFrame = static_cast<int>(frame);
+        binding.constants.iTime = time.getFTime();
+        binding.constants.iPos = position;
+        binding.constants.iDir = dir;
+        binding.constants.iUpDir = updir;
+        binding.constants.iSideDir = sideVec;
+        binding.uav(::shader::TextureTest::output, raymarched.uav());
+
+        unsigned x = static_cast<unsigned>(divideRoundUp(static_cast<uint64_t>(iRes.x), 8));
+        unsigned y = static_cast<unsigned>(divideRoundUp(static_cast<uint64_t>(iRes.y), 8));
+        node.dispatch(binding, uint3{ x, y, 1 });
+
+        if (enableAsyncCompute)
+        {
+          node.queryCounters([&](MemView<std::pair<std::string, double>> view)
+          {
+            asyncCompute.clear();
+            for (auto&& it : view)
+            {
+              asyncCompute.push_back(std::make_pair(it.first, it.second));
+            }
+          });
+        }
+
+        tasks.addPass(std::move(node));
+      }
+      //if (inputs.isPressedThisFrame('3', 2))
+
+      {
+        auto node = tasks.createPass("clear");
+        node.acquirePresentableImage(swapchain);
+        node.clearRT(backbuffer, float4{ std::sin(time.getFTime())*.5f + .5f, 0.f, 0.f, 0.f });
+        //node.clearRT(texRtv, vec4{ std::sin(float(frame)*0.01f)*.5f + .5f, std::sin(float(frame)*0.01f)*.5f + .5f, 0.f, 1.f });
+        tasks.addPass(std::move(node));
+      }
+
+      iRes.y = 1;
+      postPass.compute(tasks, texUav2, raymarched.srv(), iRes);
+      iRes.x *= 64;
+      postPass2.compute(tasks, texUav3, raymarched.srv(), iRes);
+
+      {
+        // we have pulsing red color background, draw a triangle on top of it !
+        auto node = tasks.createPass("Triangle!");
+        node.renderpass(triangleRenderpass);
+        backbuffer.setOp(LoadOp::DontCare);
+        node.subpass(backbuffer);
+        backbuffer.setOp(LoadOp::DontCare);
+
+        vector<float4> vertices;
+        vertices.push_back(float4{ -1.f, -1.f, 1.f, 1.f });
+        vertices.push_back(float4{ -1.0f, 3.f, 1.f, 1.f });
+        vertices.push_back(float4{ 3.f, -1.f, 1.f, 1.f });
+
+        auto verts = dev.dynamicBuffer(makeMemView(vertices), FormatType::Float32RGBA);
+
+        auto binding = node.bind<::shader::Triangle>(trianglePipe);
+        //binding.constants.color = float4{ 0.f, 0.f, std::sin(float(frame)*0.01f + 1.0f)*.5f + .5f, 1.f };
+        binding.constants.color = float4{ 0.f, 0.f, 0.f, 1.f };
+        binding.constants.colorspace = static_cast<int>(swapchain.impl()->displayCurve());
+        binding.srv(::shader::Triangle::vertices, verts);
+        binding.srv(::shader::Triangle::yellow, raymarched.srv());
+        node.draw(binding, 3, 1);
+        node.endRenderpass();
+        tasks.addPass(std::move(node));
+      }
+      //float heightMulti = 1.f; // float(testSrv.desc().desc.height) / float(testSrv.desc().desc.width);
+      //blit.blitImage(dev, tasks, backbuffer, testSrv, renderer::Blitter::FitMode::Fit);
+
+      auto bpos = div(uint2(backbuffer.desc().desc.width, backbuffer.desc().desc.height), 2u);
+      blit.blit(dev, tasks, backbuffer, texSrv2, int2(bpos), int2(bpos));
+      blit.blit(dev, tasks, backbuffer, texSrv3, int2(bpos.x, 0), int2(bpos));
+      */
     }
   };
 }
