d13afdb06d2b82915346ebd7193fac7a4cb046d7
Juho G <jgavert@users.noreply.github.com>
2018-06-14 16:49:11 +0300

    Fixed issue where I used buffer as RAW buffer but didn't tell the driver about it. Improvements to build system, automatic vulkan version detection, hopefully latest always. Few fixes to copy scripts to ensure everything is copied from the right places, maybe assert if files aren't found :thinking:

--- copyShaderC.bat
+++ copyShaderC.bat
@@ -7,6 +7,6 @@ IF EXIST %cachefolder% (
 	COPY %cachefolder%\build\libshaderc\RelWithDebInfo\shaderc_combined.lib externals\shaderc\lib\shaderc_combined_r.lib
 	COPY %cachefolder%\build\libshaderc\RelWithDebInfo\shaderc.lib externals\shaderc\lib\shaderc_r.lib
 	COPY %cachefolder%\build\libshaderc\Debug\shaderc_combined.lib externals\shaderc\lib\shaderc_combined_d.lib
-	COPY %cachefolder%\build\libshaderc\Debug\shadercd.lib externals\shaderc\lib\shaderc_d.lib
+	COPY %cachefolder%\build\libshaderc\Debug\shaderc.lib externals\shaderc\lib\shaderc_d.lib
 	ROBOCOPY %cachefolder%\libshaderc\include externals\shaderc\include /E
 )
