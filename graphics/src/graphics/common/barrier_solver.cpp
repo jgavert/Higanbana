95c1cdfe695717e273fcb177186e9f74a05eab52
Juho G <jgavert@users.noreply.github.com>
2019-06-14 17:22:46 +0300

    continuing

--- graphics/src/graphics/common/barrier_solver.cpp
+++ graphics/src/graphics/common/barrier_solver.cpp
@@ -10,21 +10,40 @@ namespace faze
       m_drawCallStage.push_back(baseFlags);
       return index; 
     }
-    void BarrierSolver::addBuffer(int drawCallIndex, int64_t id, ResourceHandle buffer, backend::AccessStage stage, backend::AccessUsage usage)
+    void BarrierSolver::addBuffer(int drawCallIndex, ResourceHandle buffer, ResourceState access)
     {
-
+      //m_bufferStates[buffer].state = ResourceState( backend::AccessUsage::Unknown, backend::AccessStage::Common, backend::TextureLayout::Undefined, 0);
+      m_jobs.push_back(DependencyPacket{drawCallIndex, buffer, access, SubresourceRange{}});
     }
-    void BarrierSolver::addTexture(int drawCallIndex, int64_t id, ResourceHandle texture, ResourceHandle view, int16_t mips, ResourceState access)
+    void BarrierSolver::addTexture(int drawCallIndex, ResourceHandle texture, ResourceHandle view, int16_t mips, ResourceState access)
     {
-
+      SubresourceRange range = {};
+      if (view.type == ResourceType::TextureSRV)
+      {
+        range = m_texSRVran[view];
+      }
+      else if (view.type == ResourceType::TextureUAV)
+      {
+        range = m_texUAVran[view];
+      }
+      else if (view.type == ResourceType::TextureRTV)
+      {
+        range = m_texRTVran[view];
+      }
+      else if (view.type == ResourceType::TextureDSV)
+      {
+        range = m_texDSVran[view];
+      }
+      //m_textureStates[buffer] = ResourceState( backend::AccessUsage::Unknown, backend::AccessStage::Common, backend::TextureLayout::Undefined, 0);
+      m_jobs.push_back(DependencyPacket{drawCallIndex, texture, access, range});
     }
-    void BarrierSolver::addTexture(int drawCallIndex, int64_t id, ResourceHandle texture, int16_t mips, ResourceState access, SubresourceRange range)
+    void BarrierSolver::addTexture(int drawCallIndex, ResourceHandle texture, int16_t mips, ResourceState access, SubresourceRange range)
     {
-
+      m_jobs.push_back(DependencyPacket{drawCallIndex, texture, access, range});
     }
     void BarrierSolver::reset()
     {
-
+      m_jobs.clear();
     }
   }
 }
