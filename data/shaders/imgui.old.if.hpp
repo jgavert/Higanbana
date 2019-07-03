e0dd6a2b57eb1affebc67ba0c03248a2f8d552d4
Juho G <jgavert@users.noreply.github.com>
2019-07-04 01:30:54 +0300

    old shaders for reference

---
+++ data/shaders/imgui.old.if.hpp
@@ -0,0 +1,23 @@
+#ifndef IMGUI_HLSL
+#define IMGUI_HLSL
+
+#include "definitions.hpp"
+
+FAZE_BEGIN_LAYOUT(ImGui)
+
+FAZE_CBUFFER
+{
+  float2 reciprocalResolution;
+};
+
+FAZE_SRV(ByteAddressBuffer, vertices, 0)
+FAZE_SRV(Texture2D<float>, tex, 1)
+
+FAZE_SAMPLER_POINT(pointSampler)
+
+FAZE_SRV_TABLE(2)
+FAZE_UAV_TABLE(0)
+
+FAZE_END_LAYOUT
+
+#endif /*FAZE_SHADER_DEFINITIONS*/
---
+++ data/shaders/imgui.old.vs.hlsl
@@ -0,0 +1,40 @@
+#include "imgui.if.hpp"
+
+struct PSInput
+{
+  float4 uv    : TEXCOORD0;
+  float4 color : COLOR0;
+  float4 pos   : SV_Position;
+};
+
+float4 convertUintToFloat4(uint color)
+{
+  float a = (1.0f / 255.0f)*float((color >> 24) & 0xff);
+  float b = (1.0f / 255.0f)*float((color >> 16) & 0xff);
+  float g = (1.0f / 255.0f)*float((color >> 8) & 0xff);
+  float r = (1.0f / 255.0f)*float(color & 0xff);
+  return float4(r, g, b, a);
+}
+
+GX_SIGNATURE
+PSInput main(uint id: SV_VertexID)
+{
+  uint loadID = id * 5;
+
+  const uint multiplier = 4;
+
+  float4 i1;
+  i1 = asfloat(vertices.Load4(loadID*multiplier));
+  uint col = vertices.Load((loadID + 4)*multiplier);
+
+  PSInput o;
+
+  float2 fracPos = i1.xy * reciprocalResolution;
+  float2 clipPos = lerp(float2(-1, 1), float2(1, -1), fracPos);
+
+  o.uv = float4(i1.zw, 0, 0);
+  o.color = convertUintToFloat4(col);
+  o.pos = float4(clipPos, 0, 1);
+
+  return o;
+}
