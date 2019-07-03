078b4f0f2cec879f26843f5146580fd626a88cb0
jg <jg@hidden>
2019-07-03 19:29:26 +0300

    Some slight QoL changes to scripts

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-REM set mypath=%~dp0
-REM echo %mypath%
-REM pushd %mypath%\data
-start %mypath%\bazel-bin\test_main\test_main.exe
-REM popd
+@echo off
+cd bazel-bin\test_main
+start test_main.exe
+cd ..
+cd ..
