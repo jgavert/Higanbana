74d723bee3ae588248a9939ff5d0252bed5617af
Juho G <jgavert@users.noreply.github.com>
2019-10-25 18:14:10 +0300

    Improved TLSF to handle alignments itself. Waxed the outlook and wrote few more tests. Next I think I'll try to integrate the allocator to my backends. Tests aren't really covering all cases and don't test for fragmentation. But better than nothing tbh. Still wondering how to quickly merge these kind of "blocks"...

--- tests/core/test_tlsf.cpp
+++ tests/core/test_tlsf.cpp
@@ -4,8 +4,8 @@
 #include <algorithm>
 
 struct Block {
-  size_t offset;
-  size_t size;
+  uint64_t offset;
+  uint64_t size;
   operator bool() { return size != 0; }
 };
 
@@ -21,45 +21,52 @@ class TLSFAllocator {
     std::vector<TLSFSizeClass> sizeclasses;
   };
 
-  unsigned fli;        // first level index
-  unsigned sli;        // second level index, typically 5
+  Block m_baseBlock;
+  uint64_t fli;        // first level index
+  uint64_t sli;        // second level index, typically 5
   unsigned sli_count;  // second level index, typically 5
-  unsigned mbs;        // minimum block size
-  unsigned min_fli;
+  int mbs;        // minimum block size
+  uint64_t min_fli;
   TLSFControl control;
-  size_t m_size;
+  size_t m_usedSize;
 
-  int fls(uint64_t size) {
+  int fls(uint64_t size) noexcept {
     unsigned long index;
     return _BitScanReverse64(&index, size) ? index : -1;
   }
 
-  int ffs(uint64_t size) {
+  int ffs(uint64_t size) noexcept {
     unsigned long index;
     return _BitScanForward64(&index, size) ? index : -1;
   }
 
-  void mapping(size_t size, unsigned& fl, unsigned& sl) {
+  void mapping(size_t size, uint64_t& fl, uint64_t& sl) noexcept {
     fl = fls(size);
     sl = ((size ^ (1 << fl)) >> (fl - sli));
     fl = first_level_index(fl);
     // printf("%zu -> fl %u sl %u\n", size, fl, sl);
   }
 
-  int first_level_index(unsigned fli)
-  {
-    if (fli < min_fli)
+  int first_level_index(int fli) noexcept {
+    if (fli < min_fli )
       return 0;
     return fli - min_fli;
   }
 
-  void initialize() {
-    fli = fls(m_size);
-    sli = 5;
-    sli_count = 1 << sli;
-    mbs = std::min(int(m_size), 16);
+  void initialize() noexcept {
+    fli = fls(m_baseBlock.size);
+    mbs = std::min(int(m_baseBlock.size), mbs);
     min_fli = fls(mbs);
     control.flBitmap = 0;
+    if (0)
+    {
+      size_t sizeClass = 1;
+      std::vector<std::vector<Block>> vectors;
+      for (int k = 0; k < sli_count; ++k) {
+        vectors.push_back(std::vector<Block>());
+      }
+      control.sizeclasses.push_back(TLSFSizeClass{sizeClass, 0, vectors});
+    }
     for (int i = min_fli; i <= fli; ++i) {
       size_t sizeClass = 1 << i;
       std::vector<std::vector<Block>> vectors;
@@ -70,11 +77,11 @@ class TLSFAllocator {
     }
   }
 
-  void remove_bit(uint64_t& value, int index) { value = value ^ (1 << index); }
+  void remove_bit(uint64_t& value, int index) noexcept { value = value ^ (1 << index); }
 
-  void set_bit(uint64_t& value, int index) { value |= (1 << index); }
+  void set_bit(uint64_t& value, int index) noexcept { value |= (1 << index); }
 
-  void insert(Block block, unsigned fl, unsigned sl) {
+  void insert(Block block, unsigned fl, unsigned sl) noexcept {
     auto& sizeClass = control.sizeclasses[fl];
     auto& secondLv = sizeClass.freeBlocks[sl];
     secondLv.push_back(block);
@@ -82,7 +89,7 @@ class TLSFAllocator {
     set_bit(control.flBitmap, fl);
   }
 
-  Block search_suitable_block(size_t size, unsigned fl, unsigned sl) {
+  Block search_suitable_block(size_t size, unsigned fl, unsigned sl) noexcept {
     // first step, assume we got something at fl / sl location
     auto& secondLevel = control.sizeclasses[fl];
     auto& freeblocks = secondLevel.freeBlocks[sl];
@@ -96,10 +103,11 @@ class TLSFAllocator {
     } else {
       // second step, scan bitmaps for empty slots
       // create mask to ignore first bits, could be wrong
-      auto mask = ~((1 << (fl + 1)) - 1);
+      auto mask = ~((1 << (fl+1)) - 1);
       auto fl2 = ffs(control.flBitmap & mask);
       if (fl2 >= 0) {
         auto& secondLevel2 = control.sizeclasses[fl2];
+        assert(secondLevel2.sizeClass >= size && secondLevel2.slBitmap != 0);
         auto sl2 = ffs(secondLevel2.slBitmap);
         assert(!secondLevel2.freeBlocks[sl2].empty());
         if (sl2 >= 0 && secondLevel2.freeBlocks[sl2].back().size >= size) {
@@ -117,14 +125,14 @@ class TLSFAllocator {
     return {};
   }
 
-  Block split(Block& block, size_t size) {
+  Block split(Block& block, size_t size) noexcept {
     auto new_size = block.size - size;
     Block new_block = {block.offset + size, new_size};
     block.size = size;
     return new_block;
   }
 
-  Block merge(Block block) {
+  Block merge(Block block) noexcept {
     auto otf = block.offset;
     auto otf2 = block.offset + block.size;
     // oh no, nail in the coffin. BRUTEFORCE, we got not boundary tagging
@@ -170,31 +178,54 @@ class TLSFAllocator {
   }
 
  public:
-  TLSFAllocator(size_t size) : m_size(size) {
+  TLSFAllocator(Block initialBlock, size_t minimumBlockSize = 16, int sli = 3)
+  : m_baseBlock(initialBlock), mbs(minimumBlockSize), sli(sli), sli_count(1 << sli) {
     initialize();
-    unsigned fl, sl;
-    mapping(size, fl, sl);
-    insert({0, size}, fl, sl);
+    uint64_t fl, sl;
+    mapping(m_baseBlock.size, fl, sl);
+    insert(initialBlock, fl, sl);
   }
 
-  std::optional<Block> allocate(size_t size) {
-    unsigned fl, sl, fl2, sl2;
+  TLSFAllocator(size_t size, size_t minimumBlockSize = 16, int sli = 3)
+  : m_baseBlock({0,size}), mbs(minimumBlockSize), sli(sli), sli_count(1 << sli) {
+    initialize();
+    uint64_t fl, sl;
+    mapping(m_baseBlock.size, fl, sl);
+    insert(m_baseBlock, fl, sl);
+  }
+
+  std::optional<Block> allocate(size_t size, size_t alignment = 1) noexcept {
+    size = std::max(size, size_t(mbs));
+    uint64_t fl, sl, fl2, sl2;
     mapping(size, fl, sl);
     auto found_block = search_suitable_block(size, fl, sl);
     if (found_block) {
+      auto overAlign = found_block.offset % alignment;
+      if (overAlign != 0 && found_block.size > size + alignment) {
+        // fix alignment
+        auto overAlignmentFix = alignment - overAlign;
+        auto smallFixBlock = Block{found_block.offset, overAlign};
+        found_block.offset += overAlignmentFix;
+        assert(found_block.offset % alignment == 0);
+        // guaranteed that nothing will merge with this block.
+        mapping(smallFixBlock.size, fl2, sl2);
+        insert(smallFixBlock, fl2, sl2);
+      }
       if (found_block.size > size) {
         auto remaining_block = split(found_block, size);
         mapping(remaining_block.size, fl2, sl2);
         insert(remaining_block, fl2, sl2);
       }
+      m_usedSize += found_block.size;
       return found_block;
     }
     return {};
   }
 
-  void free(Block block) {
+  void free(Block block) noexcept {
     // immediately merge with existing free blocks.
-    unsigned fl, sl;
+    m_usedSize -= block.size;
+    uint64_t fl, sl;
     auto size = block.size;
     auto big_free_block = merge(block);
     while (big_free_block.size != size)  // while loop here
@@ -205,10 +236,20 @@ class TLSFAllocator {
     mapping(big_free_block.size, fl, sl);
     insert(big_free_block, fl, sl);
   }
+
+  size_t size() const noexcept {
+    return m_baseBlock.size - m_usedSize;
+  }
+  size_t max_size() const noexcept {
+    return m_baseBlock.size;
+  }
+  size_t size_allocated() const noexcept {
+    return m_usedSize;
+  }
 };
 
 TEST_CASE("some basic allocation tests") {
-  TLSFAllocator tlsf(4);
+  TLSFAllocator tlsf(4, 1);
   auto block = tlsf.allocate(5);
   REQUIRE_FALSE(block);
   block = tlsf.allocate(2);
@@ -305,7 +346,7 @@ TEST_CASE("some basic allocation tests 2") {
 TEST_CASE("stranger tests") {
   auto count = 100;
   auto sum = (count * (count + 1)) / 2;
-  TLSFAllocator tlsf(sum);
+  TLSFAllocator tlsf(sum, 1);
   std::vector<Block> blocks;
   for (int i = 1; i <= count; ++i) {
     auto block = tlsf.allocate(i);
@@ -317,4 +358,50 @@ TEST_CASE("stranger tests") {
   tlsf.free(blocks.back());
   block = tlsf.allocate(1);
   REQUIRE(block);
+}
+
+TEST_CASE("alignment tests") {
+  TLSFAllocator tlsf(50, 1);
+  auto block = tlsf.allocate(3, 1);
+  auto block2 = tlsf.allocate(3*3*3, 9);
+  REQUIRE(block);
+  REQUIRE(block.value().offset % 1 == 0);
+  REQUIRE(block.value().size == 3);
+  REQUIRE(block2);
+  REQUIRE(block2.value().offset % 9 == 0);
+  REQUIRE(block2.value().size == 3*3*3);
+  auto block4 = tlsf.allocate(10, 2);
+  REQUIRE(block4);
+  REQUIRE(block4.value().size == 10);
+  auto block3 = tlsf.allocate(3, 1);
+  REQUIRE(block3);
+  REQUIRE(block3.value().size == 3);
+  auto block5 = tlsf.allocate(10, 1);
+  REQUIRE(block5);
+  REQUIRE(block5.value().size == 10);
+
+  tlsf.free(block2.value());
+  block2 = tlsf.allocate(3*3, 1);
+  
+  REQUIRE(block2);
+  REQUIRE(block2.value().offset % 1 == 0);
+  REQUIRE(block2.value().size == 3*3);
+}
+
+TEST_CASE("alignment tests with minimum block size") {
+  TLSFAllocator tlsf(100, 16);
+  auto block = tlsf.allocate(3, 1);
+  auto block2 = tlsf.allocate(3*3*3, 9);
+  REQUIRE(block);
+  REQUIRE(block.value().offset % 1 == 0);
+  REQUIRE(block.value().size == 16);
+  REQUIRE(block2);
+  REQUIRE(block2.value().offset % 9 == 0);
+  REQUIRE(block2.value().size == 3*3*3);
+  auto block4 = tlsf.allocate(10, 2);
+  REQUIRE(block4);
+  REQUIRE(block4.value().size == 16);
+  auto block3 = tlsf.allocate(3, 1);
+  REQUIRE(block3);
+  REQUIRE(block3.value().size == 16);
 }
