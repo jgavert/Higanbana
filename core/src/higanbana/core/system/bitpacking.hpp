f3c53b418d842a4e7ad2a57e518923204b31cb77
Juho G <jgavert@users.noreply.github.com>
2019-10-28 00:59:14 +0200

    Better atomic swap and started debugging some input problems...

--- core/src/higanbana/core/system/bitpacking.hpp
+++ core/src/higanbana/core/system/bitpacking.hpp
@@ -3,6 +3,24 @@
 
 namespace higanbana
 {
+  template <typename Type, typename Type2>
+  Type packValue(Type2 value, int offset, int bits)
+  {
+    Type val = static_cast<Type>(value);
+    Type mask = (1 << bits) - 1;
+    val = val & mask;
+    val = val << offset;
+    return val;
+  }
+
+  template <typename Type, typename Type2>
+  Type unpackValue(Type2 value, int offset, int bits)
+  {
+    Type2 val = value >> offset;
+    Type2 mask = (1 << bits) - 1;
+    val = val & mask;
+    return static_cast<Type>(val);
+  }
   int64_t packInt64(int32_t first, int32_t second);
   void unpackInt64(int64_t val, int32_t& first, int32_t& second);
 }
