edc32cddd9112791760709d110f7a00c290dceed
Juho G <jgavert@users.noreply.github.com>
2019-12-22 09:13:25 +0200

    Mutexed RabbitPools and increased queryheap sizes.

--- core/src/higanbana/core/system/MemoryPools.hpp
+++ core/src/higanbana/core/system/MemoryPools.hpp
@@ -83,21 +83,25 @@ namespace higanbana
     std::shared_ptr<vector<T>> m_pool;
     std::function<T()> m_makeItem;
     int m_size;
+    std::shared_ptr<std::mutex> m_poolMutex;
   public:
     Rabbitpool2()
       : m_pool(nullptr)
       , m_size(0)
+      , m_poolMutex(std::make_shared<std::mutex>())
     {
     }
     Rabbitpool2(std::function<T()> makeItem)
       : m_pool(std::make_shared<vector<T>>())
       , m_makeItem(makeItem)
       , m_size(0)
+      , m_poolMutex(std::make_shared<std::mutex>())
     {
     }
 
     std::shared_ptr<T> allocate() noexcept
     {
+      std::lock_guard<std::mutex> guard(*m_poolMutex);
       if (m_pool->empty())
       {
         m_pool->emplace_back(std::move(m_makeItem()));
@@ -105,12 +109,17 @@ namespace higanbana
       }
 
       std::weak_ptr<vector<T>> weakpool = m_pool;
+      std::weak_ptr<std::mutex> weakpoolMutex = m_poolMutex;
 
-      auto obj = std::shared_ptr<T>(new T(std::move(m_pool->back())), [weakpool](T* ptr)
+      auto obj = std::shared_ptr<T>(new T(std::move(m_pool->back())), [weakpool, weakpoolMutex](T* ptr)
       {
-        if (auto pool = weakpool.lock())
+        if (auto lock = weakpoolMutex.lock())
         {
-          pool->emplace_back(std::move(*ptr));
+          std::lock_guard<std::mutex> guard(*lock);
+          if (auto pool = weakpool.lock())
+          {
+            pool->emplace_back(std::move(*ptr));
+          }
         }
         delete ptr;
       });
@@ -122,6 +131,7 @@ namespace higanbana
 
     void clear() noexcept
     {
+      std::lock_guard<std::mutex> guard(*m_poolMutex);
       m_pool = std::make_shared<vector<T>>();
       m_size = 0;
     }
