466a48862d09d2d583f8f25ad6c2d36b23f49a52
Juho G <jgavert@users.noreply.github.com>
2020-05-12 22:42:07 +0300

    hmm async is broken beyond my feelings to fix it. oh well There is at least 1 random lockup issue.

--- benchmarks/core/bench_coroutine.cpp
+++ benchmarks/core/bench_coroutine.cpp
@@ -11,6 +11,113 @@
 #include <deque>
 #include <experimental/coroutine>
 
+
+struct suspend_never {
+  constexpr bool await_ready() const noexcept { return true; }
+  void await_suspend(std::experimental::coroutine_handle<>) const noexcept {}
+  constexpr void await_resume() const noexcept {}
+};
+
+struct suspend_always {
+  constexpr bool await_ready() const noexcept { return false; }
+  void await_suspend(std::experimental::coroutine_handle<>) const noexcept {}
+  constexpr void await_resume() const noexcept {}
+};
+template<typename T>
+class async_awaitable {
+public:
+  struct promise_type {
+    using coro_handle = std::experimental::coroutine_handle<promise_type>;
+    auto get_return_object() {
+      //printf("get_return_object\n");
+      return coro_handle::from_promise(*this);
+    }
+    auto initial_suspend() {
+      //-printf("initial_suspend\n");
+      return suspend_always();
+    }
+    auto final_suspend() noexcept {
+      //printf("final_suspend\n");
+      return suspend_always();
+    }
+    void return_value(T value) {m_value = value;}
+    void unhandled_exception() {
+      std::terminate();
+    }
+    auto await_transform(async_awaitable<T> handle) {
+      //printf("await_transform\n");
+      return handle;
+    }
+    T m_value;
+    std::future<void> m_fut;
+    std::promise<void> finalFut;
+    std::future<void> m_final;
+    std::weak_ptr<coro_handle> weakref;
+  };
+  using coro_handle = std::experimental::coroutine_handle<promise_type>;
+  async_awaitable(coro_handle handle) : handle_(std::shared_ptr<coro_handle>(new coro_handle(handle), [](coro_handle* ptr){ptr->destroy(); delete ptr;}))
+  {
+    //printf("created async_awaitable\n");
+    assert(handle);
+    handle_->promise().m_final = handle_->promise().finalFut.get_future();
+
+    handle_->promise().weakref = handle_;
+    handle_->promise().m_fut = std::async(std::launch::async, [handlePtr = handle_]
+    {
+      if (!handlePtr->done()) {
+        handlePtr->resume();
+        if (handlePtr->done())
+          handlePtr->promise().finalFut.set_value();
+      }
+    });
+  }
+  async_awaitable(async_awaitable& other) {
+    handle_ = other.handle_;
+  };
+  async_awaitable(async_awaitable&& other) : handle_(std::move(other.handle_)) {
+    //printf("moving\n");
+    assert(handle_);
+    other.handle_ = nullptr;
+    //other.m_fut = nullptr;
+  }
+  // coroutine meat
+  T await_resume() noexcept {
+    return handle_->promise().m_value;
+  }
+  bool await_ready() noexcept {
+    return handle_->done();
+  }
+
+  // enemy coroutine needs this coroutines result, therefore we compute it.
+  void await_suspend(coro_handle handle) noexcept {
+    handle_->promise().m_fut.wait();
+    handle_->promise().m_final.wait();
+    //handle.promise().m_fut.wait();
+    std::shared_ptr<coro_handle> otherHandle = handle.promise().weakref.lock();
+    
+    
+    auto newfut = std::async(std::launch::async, [handlePtr = otherHandle]() mutable
+    {
+      if (!handlePtr->done()) {
+        handlePtr->resume();
+        if (handlePtr->done())
+          handlePtr->promise().finalFut.set_value();
+      }
+    });
+    handle.promise().m_fut = std::move(newfut);
+  }
+  // :o
+  ~async_awaitable() { }
+  T get()
+  {
+    //printf("get\n");
+    handle_->promise().m_final.wait();
+    assert(handle_->done());
+    return handle_->promise().m_value; 
+  }
+private:
+  std::shared_ptr<coro_handle> handle_;
+};
 class Barrier
 {
   friend class BarrierObserver;
@@ -640,18 +747,6 @@ class LBSPool
 
 static std::shared_ptr<LBSPool> my_pool = nullptr;
 
-struct suspend_never {
-  constexpr bool await_ready() const noexcept { return true; }
-  void await_suspend(std::experimental::coroutine_handle<>) const noexcept {}
-  constexpr void await_resume() const noexcept {}
-};
-
-struct suspend_always {
-  constexpr bool await_ready() const noexcept { return false; }
-  void await_suspend(std::experimental::coroutine_handle<>) const noexcept {}
-  constexpr void await_resume() const noexcept {}
-};
-
 template<typename T>
 class lbs_awaitable {
 public:
@@ -767,7 +862,7 @@ namespace {
         return number < 2 ? 1 : Fibonacci(number - 1) + Fibonacci(number - 2);
     }
 
-    lbs_awaitable<uint64_t> FibonacciOrig(uint64_t number) {
+    async_awaitable<uint64_t> FibonacciOrig(uint64_t number) {
         co_return Fibonacci(number);
     }
     lbs_awaitable<uint64_t> FibonacciCoro(uint64_t number, uint64_t parallel) {
@@ -777,6 +872,18 @@ namespace {
         if (number > parallel) {
             auto v0 = FibonacciCoro(number-1, parallel);
             auto v1 = FibonacciCoro(number-2, parallel);
+            co_return co_await v0 + co_await v1;
+        }
+        co_return Fibonacci(number - 1) + Fibonacci(number - 2);
+    }
+
+    async_awaitable<uint64_t> FibonacciAsync(uint64_t number, uint64_t parallel) {
+        if (number < 2)
+            co_return 1;
+        
+        if (number > parallel) {
+            auto v0 = FibonacciAsync(number-1, parallel);
+            auto v1 = FibonacciAsync(number-2, parallel);
             co_return co_await v0 + co_await v1;
         }
         co_return Fibonacci(number - 1) + Fibonacci(number - 2);
@@ -824,11 +931,11 @@ TEST_CASE("Benchmark Fibonacci", "[benchmark]") {
         return FibonacciCoro(36,36-parallel-3).get();
     };
 
-    BENCHMARK("Fibonacci 40") {
-        return FibonacciOrig(40).get();
+    BENCHMARK("Fibonacci 38") {
+        return FibonacciOrig(38).get();
     };
-    BENCHMARK("Coroutine Fibonacci 40") {
-        return FibonacciCoro(40,40-parallel-6).get();
+    BENCHMARK("Coroutine Fibonacci 38") {
+        return FibonacciCoro(38,38-parallel-6).get();
     };
     
 
